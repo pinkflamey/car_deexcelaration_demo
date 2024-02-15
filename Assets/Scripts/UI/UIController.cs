e8cf77c6b971d76cc587d043b50c1b0f9525b4f2
Mavis de Ridder <89397118+pinkflamey@users.noreply.github.com>
2024-02-15 15:48:11 +0100

    Improved UI

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -10,28 +10,31 @@ using Slider = UnityEngine.UI.Slider;
 public class UIController : MonoBehaviour
 {
     public LevelController lc;
-    public CameraSwitcher cs;
     [Header("Distance meter")]
     public Slider distanceSlider;
     public TextMeshProUGUI distanceText;
     [Header("Acceleration setting")]
     public TMP_InputField accelerationInputField;
+    public string inputFieldText;
+    public string inputFieldTextBlinking;
     [Header("Buttons")]
     public UnityEngine.UI.Button runButton;
     public UnityEngine.UI.Button resetButton;
-    [Header("Camera switching")]
-    public TMP_Dropdown cameraDropdown;
+
+    private Coroutine blinkInputFieldCoroutine;
     
 
     private void Start()
     {
         InitializeDistanceMeter();
+        blinkInputFieldCoroutine = StartCoroutine(BlinkInputField(accelerationInputField));
     }
 
     private void Update()
     {
         UpdateDistanceMeter();
         UpdateButtons();
+        if (blinkInputFieldCoroutine == null) StartCoroutine(BlinkInputField(accelerationInputField));
     }
 
     private void InitializeDistanceMeter()
@@ -45,15 +48,15 @@ public class UIController : MonoBehaviour
     {
         var distance = Vector2.Distance(lc.carMover.frontPoint.position,
             lc.resetPoint.position);
-        if (lc.carMover.transform.position.x > lc.resetPoint.position.x)
+        if (lc.carMover.transform.position.x >= lc.resetPoint.position.x)
         {
             distanceSlider.value = 0;
-            distanceText.text = "0";
+            distanceText.text = "0m";
         }
         else
         {
             distanceSlider.value = distance;
-            distanceText.text = Mathf.Round(distance).ToString(); 
+            distanceText.text = Mathf.Round(distance).ToString() + "m"; 
         }
     }
 
@@ -70,24 +73,28 @@ public class UIController : MonoBehaviour
         if (lc.carMover.carState == CarMover.State.Running)
         {
             runButton.interactable = false;
-            var runColor = runButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color;
+            /*var runColor = runButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color;
             runButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(runColor.r, runColor.g, runColor.b, 0.5f);
+            */
             
             resetButton.interactable = true;
-            var resetColor = resetButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color;
+            /*var resetColor = resetButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color;
             resetButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(resetColor.r, resetColor.g, resetColor.b, 1f);
+            */
 
             accelerationInputField.interactable = false;
         }
         else
         {
             runButton.interactable = true;
-            var runColor = runButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color;
-            runButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(runColor.r, runColor.g, runColor.b, 1f);
+            /*var runColor = runButton.transform.GetChild(0).GetComponent<Image>().tintColor;
+            runButton.transform.GetChild(0).GetComponent<Image>().tintColor = new Color(runColor.r, runColor.g, runColor.b, 1f);
+            */
             
             resetButton.interactable = false;
-            var resetColor = resetButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color;
-            resetButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = new Color(resetColor.r, resetColor.g, resetColor.b, 0.5f);
+            /*var resetColor = resetButton.transform.GetChild(0).GetComponent<Image>().tintColor;
+            resetButton.transform.GetChild(0).GetComponent<Image>().tintColor = new Color(resetColor.r, resetColor.g, resetColor.b, 0.5f);
+            */
 
             accelerationInputField.interactable = true;
         }
@@ -103,8 +110,13 @@ public class UIController : MonoBehaviour
         lc.Reset();
     }
 
-    public void UpdateCamera()
+    private IEnumerator BlinkInputField(TMP_InputField inputField)
     {
-        cs.SwitchToCamera(cs.cameras[cameraDropdown.value]);
+        yield return new WaitForSeconds(1f);
+        inputField.placeholder.GetComponent<TextMeshProUGUI>().text = inputFieldTextBlinking;
+        yield return new WaitForSeconds(1f);
+        inputField.placeholder.GetComponent<TextMeshProUGUI>().text = inputFieldText;
+
+        blinkInputFieldCoroutine = null;
     }
 }
